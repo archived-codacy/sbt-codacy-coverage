6499b21f18ee47c4b23ed9882974693a22cb2897
Joao Machado <machado@codacy.com>
2016-07-12 17:04:31 +0100

    Bump coverageParser

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
 
   lazy val codacyScalaApi = "com.codacy" %% "codacy-api-scala" % "1.0.2"
-  lazy val coverageParser = "com.codacy" %% "coverage-parser" % "1.1.3"
+  lazy val coverageParser = "com.codacy" %% "coverage-parser" % "1.1.4"
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "2.2.4" % "test"
 
 }
