d41064ec66457868076ed3069acc81d66efbeef2
Joao Machado <machado@codacy.com>
2016-10-19 17:46:52 +0100

    Bump codacy-api-scala
    Use rapture.io to abstract json and ws

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
 
-  lazy val codacyScalaApi = "com.codacy" %% "codacy-api-scala" % "1.0.2"
+  lazy val codacyScalaApi = "com.codacy" %% "codacy-api-scala" % "2.0.0"
   lazy val coverageParser = "com.codacy" %% "coverage-parser" % "1.1.4"
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "2.2.4" % "test"
 
