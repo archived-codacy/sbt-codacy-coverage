933beeb4b454abfb088231ce89cceb271938255f
Rodrigo Fernandes <rodrigo.fernandes@tecnico.ulisboa.pt>
2016-11-02 10:07:34 +0000

    Update coverage parser 2.0.6

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
 
   lazy val codacyScalaApi = "com.codacy" %% "codacy-api-scala" % "2.0.1"
-  lazy val coverageParser = "com.codacy" %% "coverage-parser" % "2.0.0"
+  lazy val coverageParser = "com.codacy" %% "coverage-parser" % "2.0.6"
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "2.2.4" % "test"
   lazy val `rapture-json-circe` = "com.propensive" %% "rapture-json-circe" % "2.0.0-M7"
 
