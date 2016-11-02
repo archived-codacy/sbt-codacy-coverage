0597a4bbecd0f1213074807eb9b09941c902187d
Rodrigo Fernandes <rodrigo.fernandes@tecnico.ulisboa.pt>
2016-11-02 09:17:31 +0000

    Update coverage parser to 2.0.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
 
   lazy val codacyScalaApi = "com.codacy" %% "codacy-api-scala" % "2.0.1"
-  lazy val coverageParser = "com.codacy" %% "coverage-parser" % "1.1.4"
+  lazy val coverageParser = "com.codacy" %% "coverage-parser" % "2.0.0"
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "2.2.4" % "test"
 
 }
