93aa4f7e9ae4d6ef4dbfa14f52af7e39abb2aad2
Daniel Reigada <daniel@codacy.com>
2018-03-14 18:43:43 +0000

    Update codacy api scala and coverage parser

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,8 +2,8 @@ import sbt._
 
 object Dependencies {
 
-  val codacyScalaApi = "com.codacy" %% "codacy-api-scala" % "3.0.2"
-  val coverageParser = "com.codacy" %% "coverage-parser" % "2.0.2"
+  val codacyScalaApi = "com.codacy" %% "codacy-api-scala" % "3.0.7"
+  val coverageParser = "com.codacy" %% "coverage-parser" % "2.0.7"
   val `rapture-json-circe` = "com.propensive" %% "rapture-json-circe" % "2.0.0-M8" exclude("org.spire-math", "jawn-parser_2.11")
 
 }
