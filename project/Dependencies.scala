0d930c791463ac61ace4682d1d793ce9adef86aa
Luís Ferreira <contact@lsferreira.net>
2019-09-02 14:49:16 +0100

    build: sbt: fix and update configuration
    Signed-off-by: Luís Ferreira <contact@lsferreira.net>

--- project/Dependencies.scala
+++
@@ -1,8 +0,0 @@
-import sbt._
-
-object Dependencies {
-  object Codacy {
-    val coverageParser = "com.codacy" %% "coverage-parser" % "2.0.12"
-  }
-  val javaxActivation = "com.sun.activation" % "javax.activation" % "1.2.0"
-}
